a0736d10e52caa0d8a2f79b64e4742c19d28b1d3
leahlt <leahlt@gmail.com>
2019-10-27 23:03:55 -0700

    reverted alu

--- alu.v
+++ alu.v
@@ -1,10 +1,16 @@
-module ALU(Ain,Bin,ALUop,out);
+module ALU(Ain,Bin,ALUop,out, Z);
   input [15:0] Ain, Bin;
   input [1:0] ALUop;
   output [15:0] out;
+  output [2:0] Z; //if the main 16-bit result of ALU is 0, Z = 1 else Z = 0
   
   reg [15:0] result;
   assign out = result;
+  assign Z[0] = ( result === 16'b0000_0000_0000_0000 );
+  assign Z[1] = (result[15] === 1'b1);
+  assign Z[2] = ( ((Ain[15] ~^ Bin[15]) & (Bin[15] ^ result[15]) & ~ALUop[0] ) 
+                | ((Ain[15] ^ Bin[15]) & (Bin[15] == result[15]) & ALUop[0]));
+  
 
   always @(*) begin
     case(ALUop)
@@ -17,5 +23,3 @@ module ALU(Ain,Bin,ALUop,out);
   end
   
 endmodule
-
-//need to edit the output z to be a 3 bit informational output for lab 6 Z[2] = ((Ain[15] ~^ Bin[15]) & result[15]);
