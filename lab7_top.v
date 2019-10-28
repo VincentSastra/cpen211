f71b5428bbb995abc2efce7f85a50e545bc427c7
VincentSastra <vincent.sastra@gmail.com>
2019-10-27 17:17:39 -0700

    blaa

--- lab7_top.v
+++ lab7_top.v
@@ -24,8 +24,8 @@ module lab7_top (
    	cpu U(clk, reset, read_data, mem_cmd, write_data, mem_addr);
 	
 	// Tri state inverter to read   	
-	wire msel = (`MREAD == mem_cmd) && (mem_addr[8] == 1'b0);
-   	read_data = msel ? dout : 15'bzzz_zzz_zzz_zzz_zzz;
+	wire amsel = (`MREAD == mem_cmd) && (mem_addr[8] == 1'b0);
+   	assign read_data = msel ? dout : 15'bzzz_zzz_zzz_zzz_zzz;
 
    	wire write = (`MWRITE == mem_cmdm);
    	RAM MEM(clk,mem_addr[7:0],mem_addr[7:0],write, write_data,dout); // TODO idk how to connect write dout din
