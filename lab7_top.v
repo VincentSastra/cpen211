a9ac7e5dff852d971388b69bc837e368d9ef4538
VincentSastra <vincent.sastra@gmail.com>
2019-10-27 17:18:36 -0700

    typo

--- lab7_top.v
+++ lab7_top.v
@@ -18,16 +18,16 @@ module lab7_top (
 
 	wire [1:0] mem_cmd; // bit 0 is read. bit 1 is write
 	wire [8:0] mem_addr;
-	wire [15:0] read_data, write_data;
+	wire [15:0] read_data, write_data, dout;
 	
 	
    	cpu U(clk, reset, read_data, mem_cmd, write_data, mem_addr);
 	
 	// Tri state inverter to read   	
-	wire amsel = (`MREAD == mem_cmd) && (mem_addr[8] == 1'b0);
+	wire msel = (`MREAD == mem_cmd) && (mem_addr[8] == 1'b0);
    	assign read_data = msel ? dout : 15'bzzz_zzz_zzz_zzz_zzz;
 
-   	wire write = (`MWRITE == mem_cmdm);
+   	wire write = (`MWRITE == mem_cmd);
    	RAM MEM(clk,mem_addr[7:0],mem_addr[7:0],write, write_data,dout); // TODO idk how to connect write dout din
 
 
