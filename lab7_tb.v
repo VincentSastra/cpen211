309fad28856e8b0004ab743b66313e498ed992b8
VincentSastra <vincent.sastra@gmail.com>
2019-10-28 16:04:45 -0700

    finished testbench

--- lab7_tb.v
+++ lab7_tb.v
@@ -11,7 +11,7 @@ module lab7_tb();
 	
 	initial forever begin //clk
 	KEY[0] = 0;
-	#2
+	#2;
 	KEY[0] = 1;
 	#2;
 	end
@@ -20,50 +20,78 @@ module lab7_tb();
 		KEY[1] = 0; //reset
 		#4
 		KEY[1] = 1;
-		#4
-
 	err = 0;
+	    SW[9:0] = 10'b00_1010_0110;
 
-	if (DUT.CPU.PC !== 9'b0) begin err = 1; $display("FAILED: PC is not reset to zero."); $stop; end
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 1 *before* executing MOV R0, X
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 1 *before* executing MOV R0, X
-  
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 1 *before* executing MOV R0, X
 
-    if (DUT.CPU.PC !== 9'h3) begin err = 1; $display("FAILED: PC should be 1."); $stop; end
-    if (DUT.CPU.DP.REGFILE.R0 !== 16'h0007) begin err = 1; $display("FAILED: R0 should be 5."); $stop; end  // because MOV R0, X should have occurred
-
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 2 *after* executing MOV R0, X
-
-    if (DUT.CPU.PC !== 9'h4) begin err = 1; $display("FAILED: PC should be 2."); $stop; end
-    if (DUT.CPU.DP.REGFILE.R1 !== 16'h000E) begin err = 1; $display("FAILED: R0 should be 5."); $stop; end  // because MOV R0, X should have occurred
-
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 3 *after* executing LDR R1, [R0]
-
-    if (DUT.CPU.PC !== 9'h5) begin err = 1; $display("FAILED: PC should be 3."); $stop; end
-    if (DUT.CPU.DP.REGFILE.R2 !== 16'h0011) begin err = 1; $display("FAILED: R1 should be 0xABCD. Looks like your LDR isn't working."); $stop; end
-
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 4 *after* executing MOV R2, Y
-
-    if (DUT.CPU.PC !== 9'h6) begin err = 1; $display("FAILED: PC should be 4."); $stop; end
-    if (DUT.CPU.DP.Z !== 1'b1) begin err = 1; $display("FAILED: R2 should be 6."); $stop; end
-
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 5 *after* executing STR R1, [R2]
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 1 *before* executing MOV R0, X
-
-    if (DUT.CPU.PC !== 9'h7) begin err = 1; $display("FAILED: PC should be 5."); $stop; end
-    if (DUT.CPU.DP.REGFILE.R4 !== 16'b1001011110111100) begin err = 1; $display("FAILED: mem[6] wrong; looks like your STR isn't working"); $stop; end
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+	if (DUT.CPU.PC !== 9'b0) begin err = 1; $display("FAILED: PC is not reset to zero."); end // Test if PC reset to 0
     
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 4 *after* executing MOV R2, Y
 
-    if (DUT.CPU.DP.REGFILE.R5 !== ~(16'b1001011110111100)) begin err = 1; $display("FAILED: R2 should be 6."); $stop; end
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.PC !== 9'h1) begin err = 1; $display("FAILED: PC should be 1."); end // Test if PC increment
+        
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.DP.REGFILE.R0 !== 16'h0007) begin err = 1; 
+    $display("FAILED: R0 should be 7."); end  // because MOV R0, 7 should have occurred
 
-    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait here until PC changes; autograder expects PC set to 4 *after* executing MOV R2, Y
 
-    if (DUT.CPU.DP.REGFILE.R6 !== 4'h0000) begin err = 1; $display("FAILED: R2 should be 6."); $stop; end
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.DP.REGFILE.R1 !== 16'h000E) begin err = 1; 
+    $display("FAILED: R1 should be 14."); end  // because MOV R1, R0, LSL #1 should have occurred
 
-    #1000
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.DP.REGFILE.R2 !== 16'h0011) begin err = 1; 
+    $display("FAILED: R2 should be 11."); end // because ADD R2, R1, R0, LSL #1 
+
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.DP.Z !== 1'b1) begin err = 1; 
+   	$display("FAILED: Z should be 1."); end // because CMP R0, R1, LSR #1
+
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait for R3 to load WORD address; 
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait for R3 to load WORD; 
+    if (DUT.CPU.DP.REGFILE.R4 !== 16'b1001011110111100) begin err = 1; 
+    $display("FAILED: R5 should be 4'h97BC.");  end // because LDR R3, [R4]
+    
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait for R7 to load LEDR address; 
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait for R7 to load LEDR_BASE;     
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (LEDR[7:0] !== 8'b10111100) begin err = 1;
+    $display("FAILED LEDR[7:0] should be 2'hBC"); // because STR R3, [R7]
+
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.DP.REGFILE.R5 !== ~16'b1001011110111100) begin err = 1; 
+    $display("FAILED: R5 should be ~4'h97BC.");  end // because MVN R4, R3
+
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.DP.REGFILE.R6 !== 0) begin err = 1; 
+    $display("FAILED: R6 should be 0.");   end // because R3 & R4 = 0
+
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait for R7 to load SW_BASE address; 
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  // wait for R7 to load SW_BASE address;
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);  //  wait until step over; 
+    if (DUT.CPU.DP.REGFILE.R7 !== SW[7:0]) begin err = 1; 
+    $display("FAILED: R7 should be the val of SW.");  end // because LDR R6, [R7] 
+
+    @(posedge DUT.CPU.PC or negedge DUT.CPU.PC);
+   	if (DUT.CPU.con.present_state !== 6'b111000) begin err = 1; 
+    $display("Didnt go to halt state");   end // because HALT
+
+
+    #20;
+   	if (DUT.CPU.con.present_state !== 6'b111000) begin err = 1; 
+    $display("Didnt go to halt state");   end // because HALT
+    #2;
+    if (~err) begin $display("TEST PASSED"); end
     $stop;
 
+
+end
+end
+	initial begin
+		#1000
+		$stop;
+
 	end
+
 endmodule
