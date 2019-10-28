20e916ad272a6bb8d57726afef5a3d15a471d4e9
VincentSastra <vincent.sastra@gmail.com>
2019-10-27 21:09:33 -0700

    clean up old code

--- alu.v
+++ alu.v
@@ -1,16 +1,11 @@
-module ALU(Ain,Bin,ALUop,out,Z);
+module ALU(Ain,Bin,ALUop,out);
   input [15:0] Ain, Bin;
   input [1:0] ALUop;
   output [15:0] out;
-  output [2:0] Z; //if the main 16-bit result of ALU is 0, Z = 1 else Z = 0
   
   reg [15:0] result;
   assign out = result;
-  assign Z[0] = ( result === 16'b0000_0000_0000_0000 );
-  assign Z[1] = (result[15] === 1'b1);
-  assign Z[2] = ( ((Ain[15] ~^ Bin[15]) & (Bin[15] ^ result[15]) & ~ALUop[0] ) 
-                | ((Ain[15] ^ Bin[15]) & (Bin[15] == result[15]) & ALUop[0])); //unsure if overflow matters in op 10 and op 11
-  
+
   always @(*) begin
     case(ALUop)
 	  2'b00: result = Ain + Bin;
